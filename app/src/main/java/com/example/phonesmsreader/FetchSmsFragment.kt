29696b3b5da77d4df59441cb24d9c25a0ab4ad78
Shohil <Sohilupadhyay47@gmail.com>
2021-09-08 11:08:28 +0530

    Done All

--- app/src/main/java/com/example/phonesmsreader/FetchSmsFragment.kt
+++ app/src/main/java/com/example/phonesmsreader/FetchSmsFragment.kt
@@ -1,23 +1,83 @@
 package com.example.phonesmsreader
 
+import android.content.ContentResolver
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import android.widget.SimpleCursorAdapter
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_fetch_sms.*
+import kotlinx.android.synthetic.main.row.*
+
 
 class FetchSmsFragment : Fragment(R.layout.fragment_fetch_sms) {
 
-    private lateinit var navController: NavController
+    // Cursor Adapter
+    var adapter: SimpleCursorAdapter? = null
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Create Inbox box URI
 
-        navController = Navigation.findNavController(view)
 
-        var phone = arguments?.getString("Phone")
-        var numberOfDays = arguments?.getString("Days")
+        // Create Inbox box URI
+        val inboxURI = Uri.parse("content://sms/inbox")
+
+        // List required columns
+
+        var id = arguments?.getString("id")
+        var days = arguments?.getString("days")
+
+
+
+
+        // List required columns
+        val reqCols = arrayOf("_id", "address", "body")
+
+        // Get Content Resolver object, which will deal with Content
+        // Provider
+        val cr: ContentResolver = requireActivity().getContentResolver()
+
+        // Fetch Inbox SMS Message from Built-in Content Provider
+
+        // Fetch Inbox SMS Message from Built-in Content Provider
+        val c = cr.query(inboxURI, reqCols, "address= '$id'", null, null, null)
+
+        // Attached Cursor with adapter and display in listview
+
+        if (c != null) {
+            while (c.moveToNext()) {
+
+                var totalMessage =1
+                while (c.moveToNext()) {
+                    val messageBody = c.getString(c.getColumnIndex("body"))
+                    val messageLength = messageBody.length.toLong()
+                    val numberOfMessages = messageLength / 160.0
+                    val numberOfMessagesRoundedUp = Math.ceil(numberOfMessages)
+                     totalMessage = (totalMessage+ numberOfMessagesRoundedUp).toInt()
+                }
+
+                if(totalMessage!=0){
+                    Toast.makeText(requireContext(), "Total Message Count is $totalMessage", Toast.LENGTH_LONG).show()
+                }else if(totalMessage==0){
+                    Toast.makeText(requireContext(), "Number of count is 0", Toast.LENGTH_LONG).show()
+                }
+
+            }
+        }
+
+
+        // Attached Cursor with adapter and display in listview
+        adapter = SimpleCursorAdapter(requireContext(), R.layout.row, c, arrayOf("body", "address"), intArrayOf(
+                R.id.lblMsg, R.id.lblNumber))
+        lvMsg.adapter = adapter
+
+
     }
-
 }
+
+
+
--- app/src/main/java/com/example/phonesmsreader/SearchFragment.kt
+++ app/src/main/java/com/example/phonesmsreader/SearchFragment.kt
@@ -1,33 +1,43 @@
 package com.example.phonesmsreader
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.Telephony
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_search.*
+import java.util.jar.Manifest
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
-
     private lateinit var navController: NavController
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        navController = Navigation.findNavController(view)
 
         btnLaunchSecondFragment.setOnClickListener {
 
 
-            val bundle = bundleOf("Phone" to etPhone.text.toString(),
-                "Days" to etDays.text.toString())
+            navController = Navigation.findNavController(view)
 
+            val bundle = bundleOf("id" to etPhone.text.toString(),
+                    "days" to etDays.text.toString())
 
             navController.navigate(R.id.action_searchFragment_to_fetchSmsFragment,bundle)
 
         }
     }
+
 }
+           
+
